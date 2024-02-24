402a40a4959b32f711c58f627a2ebd5088923e9b
Riturajsen <pulaksingh1998@gmail.com>
2024-02-24 17:21:14 +0530

    new file:   app/assets/images/logo.jpg 	new file:   forgotPass.php 	modified:   index.php

--- index.php
+++ index.php
@@ -8,15 +8,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Login System</title>
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>IIAHM Auth System</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
+<style>
+  img{
+    width: 200px;
+    height: 120px;
+  }
+</style>
 </head>
 <body>
   <div class="container mt-5">
     <div class="card p-3 shadow-lg">
-    <div class="row justify-content-center">
+      <div class="text-center"><img src="app/assets/images/logo.jpg" alt="LOGO"></div>
+    <hr>
+      <div class="row justify-content-center">
       <div class="col-md-6">
     <h2><u>Login</u></h2>
     <form action="authenticate.php" method="POST">
@@ -28,13 +39,12 @@
         <label for="password">Password:</label>
         <input type="password" class="form-control" id="password" name="password" required>
       </div>
-      <button type="submit" class="btn btn-primary">Login</button>
+      <button type="submit" class="btn btn-primary form-control">Login</button>
 
     </form>
+    <hr>
+    <div class="text-center">IIAHM © <?=date('Y')?>. All Rights Reserved </div>
 
-    <a href="website/"><button class="btn btn-success mt-3">Go To Website</button></a>
-    <a href="jobs/"><button class="btn btn-success mt-3">Go To jobs</button></a>
-    <a href="form/"><button class="btn btn-success mt-3">Go To Form</button></a>
     </div> 
     </div>
     </div>
