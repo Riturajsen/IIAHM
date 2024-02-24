a7867505e9ee206c1a0ca8632083b189315a138d
Riturajsen <pulaksingh1998@gmail.com>
2024-02-24 17:54:39 +0530

    modified:   forgotP.php

--- forgotP.php
+++ forgotP.php
@@ -3,7 +3,7 @@ session_start();
 error_reporting(false);
 include "app/config/connect.php";
 
-
+$token = md5(uniqid().rand(1000000, 9999999)).rand(10000,99999).uniqid("_iiahm_");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $email = $_POST["email"];
@@ -12,7 +12,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
 
   if (mysqli_num_rows($result) == 1) {
-        echo "Found A account";
+    echo $token;
+    // $secure_query = "UPDATE `users` SET `token`='$token' WHERE email ='$email'";
+    // $sec_res = mysqli_query($conn,$secure_query);
   } else {
     echo "No Account Found Associated with This Email";
   }
