acbfcdff7ffd70b1abf36f7bac4d1ad5e18b3247
Riturajsen <pulaksingh1998@gmail.com>
2024-03-01 17:37:25 +0530

    modified:   website/assets/Content/header.php

--- website/assets/Content/header.php
+++ website/assets/Content/header.php
@@ -6,7 +6,7 @@ $registerLink = "https://form.iiahmaviationacademy.com/?utm_source=website_rgstr
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>IIAHM - Aviation Academy</title>
+    <title>IIAHM - Aviation Academy <?=$_GET['page-name']?></title>
     <!-- fontawesome css link -->
     <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
     <!-- flaticon css -->
