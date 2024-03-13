a98c71eed42ab27d298aba96df81fb72ba2a3094
Riturajsen <pulaksingh1998@gmail.com>
2024-03-13 17:46:26 +0530

    modified:   erp/allot.php 	new file:   erp/helper/fetchDataTable.php 	modified:   erp/helper/header.php

--- erp/allot.php
+++ erp/allot.php
@@ -26,22 +26,79 @@ if (mysqli_num_rows($fetchAuth) > 0){
             <div id="layoutSidenav_content">
             <!-- Display status message -->
 
-<div class="container">
+<div class="">
 <div class="row p-3">
     <!-- Import link -->
-  <?=$tele_num?>
+    <form id="myForm" action="" method="post">
+    <select name="teleCaller" class="form-control bg-dark text-white" id="teleCaller">
+     
+    <?php
+  $ret = mysqli_query($conn , "SELECT * FROM  users where rights=4");
+  if(mysqli_num_rows($ret) > 0) {
+    ?>   <option value="">Select A Caller</option>
+    <?php
+       $i =1;
+    while ($row=mysqli_fetch_array($ret)){
+    ?>
+<option value="<?=$row['id']?>"><?=$row['username']?></option>
+
+
+<?php } } else{ echo " <option value=''>No Caller Added</option>";} ?>
+    </select></form>
     <!-- Excel file upload form -->
  
-
     <!-- Data list table --> 
+     
 
 </div>
-            </div>
+<form action="helper/fetchDataTable.php" method="post">
+<input type="hidden" name="TeleId" id="TeleId">
+<table class="table table-hover">
+                                    <thead>
+            <tr>
+                <th>#</th>
+                <th>Full Name</th>
+                <th>Phone</th>
+                <th>Allot</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php 
+        // Get member rows 
+        $result = $con->query("SELECT * FROM studentdetails where allotedTo IS NULL OR allotedTo = ' 'ORDER BY id DESC "); 
+        if($result->num_rows > 0){ $i=0; 
+            while($row = $result->fetch_assoc()){ $i++; 
+        ?>
+            <tr>
+                <td><?php echo $i; ?></td>
+                <td><?php echo $row['fname']; ?></td>
+                <td><?php echo $row['contactno']; ?></td>
+                <td><input type="checkbox" name="allotedid[]" value="<?=$row['id']?>" ></td>
+        <?php } }else{ ?>
+            <tr><td colspan="4">No member(s) found...</td></tr>
+        <?php } ?>
+        </tbody>
+        <input type="submit" class="form-control bg-dark text-white"  value="Assign">
+        <hr>
+        </form>
+    </table>
+  </div>
         
-            </div>
+
+
+
+
+</div>
              <!-- footer start-->
              <?php include ('helper/footer.php'); ?>
             <!-- footer End -->
+     <script type="text/javascript">
+       var getId = document.getElementById('teleCaller');
+       getId.addEventListener
+
+       getId.addEventListener("click",(event) => {
+        document.getElementById('TeleId').value = getId.value; });
+     </script>
    
             </body>
 </html>
---
+++ erp/helper/fetchDataTable.php
@@ -0,0 +1,28 @@
+<?php 
+
+include "../../core/main.php";
+
+$TeleId = $_POST['TeleId'];
+$allotedid = $_POST['allotedid'];
+
+
+// echo "<pre>";
+// print_r($);
+// Use json_encode() function  
+
+  
+// Display the output  
+// $json = json_encode($allotedid);  
+// $output = $json;  
+// $int_var = preg_replace('/[^0-9]/', '', $output);   
+//    // print output of function 
+//    echo("The numbers are: $int_var \n");  
+  
+
+
+foreach($allotedid as $alloted){
+    $con->query("UPDATE studentdetails SET allotedTo = '".$TeleId."' WHERE id = '".$alloted."'"); 
+}
+
+header('location: ../allot.php');
+?>
